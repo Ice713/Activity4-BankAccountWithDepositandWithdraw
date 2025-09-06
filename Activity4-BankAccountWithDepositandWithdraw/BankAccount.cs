7cba665d0026c347f4324869c24d3f571b3cb4c5
unknown <icramzilruiz@gmail.com>
2025-09-06 22:57:25 +0800

    Add BankAccount class and enhance Form1 UI
    Updated project file to include BankAccount.cs and Form1.resx.
    Enhanced Form1 with new UI components and event handlers.
    Introduced validation in BankAccount for deposits and withdrawals.

---
+++ Activity4-BankAccountWithDepositandWithdraw/BankAccount.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Activity4_BankAccountWithDepositandWithdraw
+{
+    internal class BankAccount
+    {
+        private decimal balance;
+        public BankAccount(decimal initialBalance)
+        {
+            if (initialBalance < 0)
+            {
+                throw new ArgumentException("Initial balance cannot be negative.");
+            }
+            balance = initialBalance;
+        }
+        public decimal Balance
+        {
+            get { return balance; }
+        }
+        public void Deposit(decimal amount)
+        {
+            if (amount <= 0)
+            {
+                throw new ArgumentException("Deposit amount must be positive.");
+            }
+            balance += amount;
+        }
+        public void Withdraw(decimal amount)
+        {
+            if (amount <= 0)
+            {
+                throw new ArgumentException("Withdrawal amount must be positive.");
+            }
+            if (amount > balance)
+            {
+                throw new InvalidOperationException("Insufficient funds for this withdrawal.");
+            }
+            balance -= amount;
+        }
+    }
+}
--- Activity4-BankAccountWithDepositandWithdraw/Form1.Designer.cs
+++ Activity4-BankAccountWithDepositandWithdraw/Form1.Designer.cs
@@ -28,13 +28,107 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.buttonProceed = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.buttonBalance = new System.Windows.Forms.Button();
+            this.numericUpDownAmount = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownAmount)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(203, 55);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(81, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Transaction:";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Deposit",
+            "Withdraw"});
+            this.comboBox1.Location = new System.Drawing.Point(304, 52);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(144, 24);
+            this.comboBox1.TabIndex = 1;
+            // 
+            // buttonProceed
+            // 
+            this.buttonProceed.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.buttonProceed.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonProceed.Location = new System.Drawing.Point(251, 121);
+            this.buttonProceed.Name = "buttonProceed";
+            this.buttonProceed.Size = new System.Drawing.Size(151, 32);
+            this.buttonProceed.TabIndex = 3;
+            this.buttonProceed.Text = "Proceed";
+            this.buttonProceed.UseVisualStyleBackColor = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(203, 85);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(55, 16);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Amount:";
+            // 
+            // buttonBalance
+            // 
+            this.buttonBalance.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.buttonBalance.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonBalance.Location = new System.Drawing.Point(251, 159);
+            this.buttonBalance.Name = "buttonBalance";
+            this.buttonBalance.Size = new System.Drawing.Size(151, 38);
+            this.buttonBalance.TabIndex = 5;
+            this.buttonBalance.Text = "Check Balance";
+            this.buttonBalance.UseVisualStyleBackColor = false;
+            this.buttonBalance.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // numericUpDownAmount
+            // 
+            this.numericUpDownAmount.DecimalPlaces = 2;
+            this.numericUpDownAmount.Increment = new decimal(new int[] {
+            1,
+            0,
+            0,
+            131072});
+            this.numericUpDownAmount.Location = new System.Drawing.Point(304, 83);
+            this.numericUpDownAmount.Name = "numericUpDownAmount";
+            this.numericUpDownAmount.Size = new System.Drawing.Size(144, 22);
+            this.numericUpDownAmount.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.numericUpDownAmount);
+            this.Controls.Add(this.buttonBalance);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.buttonProceed);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownAmount)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button buttonProceed;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button buttonBalance;
+        private System.Windows.Forms.NumericUpDown numericUpDownAmount;
     }
 }
 
